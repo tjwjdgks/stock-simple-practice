96ee890a7edfaa9f97d8aa651dd4fa90af767526
tjwjdgks <tjwjdgks43@gmail.com>
2023-10-10 15:50:03 +0900

    feat: solve using named lock

---
+++ src/main/java/com/example/simple_stock/facade/NamedLockStockFacade.java
@@ -0,0 +1,26 @@
+package com.example.simple_stock.facade;
+
+import com.example.simple_stock.repository.LockRepository;
+import com.example.simple_stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class NamedLockStockFacade {
+
+    private final LockRepository lockRepository;
+    private final StockService stockService;
+
+    @Transactional
+    public void decrease(Long id, Long quantity){
+        try{
+            lockRepository.getLock(id.toString());
+            stockService.decrease(id, quantity);
+        } finally {
+            lockRepository.releaseLock(id.toString());
+        }
+    }
+
+}
--- src/main/java/com/example/simple_stock/service/StockService.java
+++ src/main/java/com/example/simple_stock/service/StockService.java
@@ -4,6 +4,7 @@ import com.example.simple_stock.domain.Stock;
 import com.example.simple_stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -12,8 +13,8 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-    //@Transactional이 경우 decrease가 wrapping되기 때문에, 트랜잭션이 커밋되기 전에 decrease가 호출될 수 있다.
-    public synchronized void decrease(Long id, Long quantity) {
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
         stockRepository.saveAndFlush(stock);
